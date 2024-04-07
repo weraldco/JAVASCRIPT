4762bec4d3c7ee6af4a63989abab8dfccd509b28
weraldco <werald.opolento@gmail.com>
2024-04-08 05:52:43 +0800

    Test

--- javascript-lesson-14-starting-code-main/scripts/amazon.js
+++ javascript-lesson-14-starting-code-main/scripts/amazon.js
@@ -1,5 +1,6 @@
-import {cart} from '../data/cart.js';
+import { cart } from '../data/cart.js';
 //import {cart as myCart} from '../data/cart.js'
+import { products } from '../data/products.js';
 
 let productsHTML = '';
 
